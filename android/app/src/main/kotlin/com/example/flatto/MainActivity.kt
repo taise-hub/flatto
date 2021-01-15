79e99b8608aae7af4a5e34591b707d0e1c2aa2f4
井上大誠 <t.base.8901@icloud.com>
2021-01-15 23:00:36 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/example/flatto/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flatto
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'flatto',
+    home: MyStatefulWidget(),
+  ));
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  MyStatefulWidget({Key key}) : super(key: key);
+
+  @override
+  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  PageController _pageController;
+  int _selectedIndex = 0;
+
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+
+  @override
+  void initState() {
+    super.initState();
+    // PageControllerの初期化
+    _pageController = PageController(initialPage: _selectedIndex);
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('フラッと')),
+      body: PageView(
+          //TODO: pageViwqのコントローラ属性について勉強する
+          controller: _pageController,
+          children: [
+            //ここにWidgetを突っ込む
+            Text(
+              'Index:1',
+              style: optionStyle,
+            ),
+            Text(
+              'Index:2',
+              style: optionStyle,
+            ),
+            Text(
+              'Index:3',
+              style: optionStyle,
+            )
+          ]),
+      bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _selectedIndex,
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.business), label: 'Bussiness'),
+            BottomNavigationBarItem(icon: Icon(Icons.school), label: 'School'),
+          ],
+          selectedItemColor: Colors.amber[80],
+          onTap: (index) {
+            setState(() {
+              _selectedIndex = index;
+              _pageController.animateToPage(index,
+                  duration: Duration(milliseconds: 300), curve: Curves.easeIn);
+            });
+          }),
+    );
+  }
+}
+
+//TODO: PageControllerの仕組みを勉強する

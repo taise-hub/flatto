edb12837003113956162d43211fee9250749ecc2
井上大誠 <t.base.8901@icloud.com>
2021-01-16 20:14:16 +0900

    add theme.dart

---
+++ lib/common/theme.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+final appTheme = ThemeData(
+  primaryColor: Color.fromRGBO(255, 204, 1, 1),
+  textTheme: TextTheme(
+    headline1: TextStyle(
+      fontFamily: 'Corben',
+      fontWeight: FontWeight.w700,
+      fontSize: 24,
+      color: Colors.black,
+    ),
+  ),
+);

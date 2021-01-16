b5d9498857d5a900c4914a03349aa3af4b18410f
井上大誠 <t.base.8901@icloud.com>
2021-01-16 23:37:20 +0900

    よいしょgit add .

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -10,7 +10,7 @@ import GoogleMaps
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
 
-    GMSServices.provideAPIKey("AIzaSyCUQLaWXiyKaEz8x3sPJ4D0hCee5-1SKgs")
+    GMSServices.provideAPIKey("secret")
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
